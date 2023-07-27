aaa30adcf3bac266627368f679e8736b54573e90
Siddhesh Ranade <ranade.siddhesh@gmail.com>
2023-07-27 19:10:59 -0400

    make header and footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,16 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
   <div className={styles.footerParent}>
-    <div>
+    <div className={styles.footerLogo}>
       <img src='../../public/images/logo-white.svg'/>
     </div>
-    <div className={styles.footerText}>© 2023 dot.cards text task. All rights reserved</div>
     <div className={styles.footerIcons}>
       <img src='../../public/images/instagram.svg'/>
       <img src='../../public/images/twitter.svg'/>
       <img src='../../public/images/youtube.svg'/>
+    </div>
+    <div className={styles.footerText}>
+      <p>© 2023 dot.cards text task. All rights reserved</p>
     </div>
   </div>
   )
